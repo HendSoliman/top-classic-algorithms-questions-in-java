90d296f1dab306a364acc021b787574a0ee74fee
hend.soliman@dxc.com <Myfavorite@2015>
2025-12-12 19:33:29 +0400

    Refactor package to com.refresh.algorithms

--- src/main/java/com/refresh/algorithms/Main.java
+++ src/main/java/com/refresh/algorithms/Main.java
@@ -1,4 +1,4 @@
-package com.refresh.mind;
+package com.refresh.algorithms;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
